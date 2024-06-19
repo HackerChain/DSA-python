20bfea38c1cf24b3060b7e39ec9a816a84a82ab4
HackerChain <jasonsmith0614.dev@gmail.com>
2024-06-19 23:58:59 +0530

    added push efficient and pop efficient stack

--- easy_dsa/DS/Stack/pop_efficient_stack.py
+++ easy_dsa/DS/Stack/pop_efficient_stack.py
@@ -1,41 +1,32 @@
 from queue import Queue
 
-class PushEfficientStack:
+class PopEfficientStack:
     def __init__(self):
         self.queue1 = Queue()
         self.queue2 = Queue()
 
     def is_empty(self):
         "Method to check if stack is empty or not"
-        return self.queue1.empty()
+        return self.queue1.empty() and self.queue2.empty()
 
     def push(self, data):
         "Method to push new node in the stack"
-        self.queue1.put(data)
+        self.queue2.put(data)
+        while not self.queue1.empty():
+            self.queue2.put(self.queue1.get())
+        self.queue1, self.queue2 = self.queue2, self.queue1  # Always keep queue1 as the main queue
 
     def pop(self):
         "Method to pop the top node in the stack"
         if self.is_empty():
             raise IndexError("pop from empty stack")
-        # Transfer all elements except the last one to queue2
-        while self.queue1.qsize() > 1:
-            self.queue2.put(self.queue1.get())
-        popped_item = self.queue1.get()
-        # Swap the names of the two queues
-        self.queue1, self.queue2 = self.queue2, self.queue1
-        return popped_item
+        return self.queue1.get()
 
     def peek(self):
         "Method to check top node"
         if self.is_empty():
             raise IndexError("peek from empty stack")
-        # Transfer all elements except the last one to queue2
-        while self.queue1.qsize() > 1:
-            self.queue2.put(self.queue1.get())
-        top_item = self.queue1.get()
-        self.queue2.put(top_item)
-        self.queue1, self.queue2 = self.queue2, self.queue1
-        return top_item
+        return self.queue1.queue[0]
 
     def get_size(self):
         "Method to get size of the stack"
